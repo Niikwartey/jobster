1319a019b524249f7e70d237b092f85c5dd97974
Derick Quartey <niikwartey1@gmail.com>
2016-07-27 10:14:00 -0400

    progressbar and working fine

--- lib/jobster/cli.rb
+++ lib/jobster/cli.rb
@@ -4,13 +4,25 @@ require 'formatador'
 class Jobster::CLI
 
   def call
-    list_jobs
-    menu
-        total = 1000
-    progress = ProgressBar.new(total, :color => "light_blue")
+    
+    
+    # Formatador.display_line("+++++++++++I'm working+++++++++")
+    # total = 100
+    # progress = Formatador::ProgressBar.new(total, :color => "gray")
+    # 1000.times do
+    #   progress.increment
+    # end
+
+    total    = 10
+    progress = Formatador::ProgressBar.new(total, :color => "green")
     1000.times do
       progress.increment
-      end      
+    end
+
+
+
+    list_jobs
+    menu
     goodbye
   end
 
@@ -20,26 +32,44 @@ class Jobster::CLI
     1. Java Developer - NY
     2. PHP Developer - NY
     DOC
+    table_data = [
+      {:title => "Java Backend Developer", :location => "New York", :short_desc => "whvfvzc xvjzx whvfbjkvhjdkb dvxk zxgjkv zxvjz xvgzkxhjv xvjzx whvfbjkvhjdkb dvxk zxgjkv zxvjz xvgzkxhjv gzhjkx vgjzc gxvzxjvzc xvjzx"},
+      {:title => "Senior Fullstack Engineer", :location => "San Francisco", :short_desc => "whvfbjkvhjdkb dvxk zxgjkv zxvjz xvgzkxhjv gzhjkx vgjzc gxvzxjvzc xvjzx"}, 
+      {:title => "Junior Frontend Developer", :location => "Los Angeles", :short_desc => "whvfbjkvhjdkb dvxk zxgjkv zxvjz xvgzkxhjv gzhjkx vgjzc gxvzxjvzc xvjzx"}
+    ]
+    table_data.each_with_index {|hash, index| hash["".to_sym] = index + 1}
+    Formatador.display_table(table_data, ["".to_sym, :title, :location, :short_desc])
   end
 
   def menu
+    table_data = [
+      {:title => "Java Backend Developer", :location => "New York", :short_desc => "whvfbjkvhjdkb dvxk zxgjkv zxvjzjkvhjdkb dvxk zxgjkv zxvjz xvgzkxhjv gzhjkx vgjzc gxvzxjvzc xvjzx whvfbjkvhjdkb dvxk zxgjkv zxvjz xvgzkxhjv gzhjkx vgjzc gxvzxjvzc xvjzx whvfbjkvhjdkb dvxk zxgjkv zxvjz xvgzkxhjv gzhjkx vgjzc gxvzxjvzc xvjzx"},
+      {:title => "Senior Fullstack Engineer", :location => "San Francisco", :short_desc => "whvfbjkvhjdkb dvxk zxgjkv zxvjz xvgzkxhjv gzhjkx vgjzc gxvzxjvzc xvjzx"}, 
+      {:title => "Junior Frontend Developer", :location => "Los Angeles", :short_desc => "whvfbjkvhjdkb dvxk zxgjkv zxvjz xvgzkxhjv gzhjkx vgjzc gxvzxjvzc xvjzx"}
+    ]
+
     puts "Enter the number of the job you would like more information on or type exit to enter:"
-    input = nil
+    input = gets.strip.downcase
 
     
-    while input != "exit"
-    input = gets.strip.downcase
-    case input
-      when "1"
-        puts "More info on job 1"
-      when "2"
-        puts "More info on job 2"
-      end
+    until input == "exit"
+      iindex = input.to_i - 1
+      table_data[i][:short_desc]
     end
+       
+    #  end input != "exit"
+    # input = gets.strip.downcase
+    # case input
+    #   when "1"
+    #     puts "More info on job 1"
+    #   when "2"
+    #     puts "More info on job 2"
+    #   end
+    # end
+
   end
 
   def goodbye
     puts "See you tomorrow for more jobs"
   end
-
 end
